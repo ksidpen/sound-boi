c25c6397a0bca2f04043500d4c73025d3bbddefa
Dennis Pawlik <dennis.pawlik@arvato.com>
2019-03-18 17:12:44 +0100

    added post route to configure board remotely

--- index.js
+++ index.js
@@ -10,18 +10,41 @@ try {
 }
 
 const config = {
+  port: process.env.PORT || 8080,
   sound: {
     cachePath: process.env.CACHE_PATH ||  'cache',
   }
 }
 
+const fastify = require('fastify')()
 const SoundBoard = require("./lib/board").SoundBoard;
 
 const main = async (config) => {
-  let board = new SoundBoard(config);
   let sample = require("./sample/board-config-1.js");
+  let board = new SoundBoard(config);
   await board.start();
   await board.load(sample);
+
+  // configure board remotely
+  let boardConfigRoute = '/'
+  fastify.post(boardConfigRoute, async (req, reply) => {
+    let newBoardConfig = req.body
+
+    if (!newBoardConfig)
+      reply.status(422).send({message: 'no board config provided'})
+    
+    if (!newBoardConfig.boards)
+      reply.status(422).send({message: 'no boards in board config provided'})
+
+    if (!newBoardConfig.boards.some(board=> board.length === 8 && board[0].length === 8))
+      reply.status(422).send({message: 'contains invalid board configs'})
+
+    await board.load(newBoardConfig);
+
+    reply.send({ status: true })
+  })
+  await fastify.listen(config.port)
+  console.log('listeneing on', config.port, boardConfigRoute)
 };
 
 main(config).catch(console.error);
