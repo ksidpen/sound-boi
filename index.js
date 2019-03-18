3b3587a67b4e35baa25d40d4072870159add5c5b
Dennis Pawlik <dennis.pawlik@arvato.com>
2019-03-18 16:30:33 +0100

    expose env config

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const SoundBoard = require('./lib/board').SoundBoard
+const dotenv = require("dotenv");
 
-const main = async () => {
-  let board = new SoundBoard();
+try {
+  let result = dotenv.config();
+  if (result.error) {
+    throw result.error;
+  }
+} catch (err) {
+  console.error('no .env file');
+}
+
+const config = {
+  sound: {
+    cachePath: process.env.CACHE_PATH ||  'cache',
+  }
+}
+
+const SoundBoard = require("./lib/board").SoundBoard;
+
+const main = async (config) => {
+  let board = new SoundBoard(config);
   let sample = require("./sample/board-config-1.js");
   await board.start();
   await board.load(sample);
 };
 
-main().catch(console.error);
+main(config).catch(console.error);
